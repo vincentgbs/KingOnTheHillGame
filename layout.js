2fcbc7afd18a346fb4858b18f391ea7cdcf019aa
vincent <vincent.gbs11@gmail.com>
2020-07-05 12:06:14 -0400

    Adding basic drawing features, no erase, need to figure out piece movements

--- layout.js
+++ layout.js
@@ -21,14 +21,26 @@ var canvas = {
             this.ctx.stroke();
         }
     },
-    draw: function() {
-        // this.ctx.strokeStyle = 'blue';
-        // this.ctx.lineWidth = 5;
-        // this.ctx.strokeRect(1,1,100,300);
-
-        // this.ctx.beginPath();
-        // this.ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-        // this.ctx.stroke();
+    draw_base: function(location) {
+        this.ctx.fillStyle = 'blue';
+        this.ctx.fillRect(location.h*100,location.v*100,99,99);
+    },
+    draw_mid: function(location) {
+        this.ctx.fillStyle = 'green';
+        this.ctx.fillRect(location.h*100 + 10,location.v*100 + 10,79,79);
+    },
+    draw_top: function(location) {
+        this.ctx.fillStyle = 'yellow';
+        this.ctx.fillRect(location.h*100 + 20,location.v*100 + 20,59,59);
+    },
+    draw_cap: function(location) {
+        this.ctx.fillStyle = 'red';
+        this.ctx.fillRect(location.h*100 + 30,location.v*100 + 30,39,39);
+    },
+    draw_piece: function(location) {
+        this.ctx.beginPath();
+        this.ctx.arc(location.h*50, location.v*50, 25, 0, 2 * Math.PI);
+        this.ctx.stroke();
     }
 }
 
@@ -40,4 +52,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // Default Starting Point
     canvas.resize(game.settings.vertical * 100, game.settings.horizontal * 100);
     canvas.draw_board();
+    canvas.draw_base({h:1, v:1});
+    canvas.draw_mid({h:1, v:1});
+    canvas.draw_top({h:1, v:1});
+    canvas.draw_cap({h:1, v:1});
+
+    canvas.draw_piece({h:1, v:1});
 });
