f8ffc34d43ac096d0f6e01d0808ca25c976d3e08
vincent <vincent.gbs11@gmail.com>
2020-07-05 11:55:11 -0400

    Drawing board

--- layout.js
+++ layout.js
@@ -6,6 +6,29 @@ var canvas = {
     resize: function (height, width) {
         this.c.height = height;
         this.c.width = width;
+    },
+    draw_board: function() {
+        for(var i = 1; i <= game.settings.vertical; i++) {
+            this.ctx.beginPath();
+            this.ctx.moveTo(i * 100, 0);
+            this.ctx.lineTo(i * 100, game.settings.horizontal * 100);
+            this.ctx.stroke();
+        }
+        for(var i = 1; i <= game.settings.horizontal; i++) {
+            this.ctx.beginPath();
+            this.ctx.moveTo(0, i * 100);
+            this.ctx.lineTo(game.settings.vertical * 100, i * 100);
+            this.ctx.stroke();
+        }
+    },
+    draw: function() {
+        // this.ctx.strokeStyle = 'blue';
+        // this.ctx.lineWidth = 5;
+        // this.ctx.strokeRect(1,1,100,300);
+
+        // this.ctx.beginPath();
+        // this.ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+        // this.ctx.stroke();
     }
 }
 
@@ -14,5 +37,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const c = document.querySelector("#board");
     const ctx = c.getContext("2d");
     canvas.set(c, ctx);
-    canvas.resize(window.innerHeight - 20, window.innerWidth - 20);
+    // Default Starting Point
+    canvas.resize(game.settings.vertical * 100, game.settings.horizontal * 100);
+    canvas.draw_board();
 });
