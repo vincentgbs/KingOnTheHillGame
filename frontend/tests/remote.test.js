f6e1e34c60b28effc953e3c769308430ecee41fb
vincent <vincent.gbs11@gmail.com>
2020-10-15 11:16:04 -0400

    Working on remote tests

--- frontend/tests/remote.test.js
+++ frontend/tests/remote.test.js
@@ -1,46 +1,41 @@
 let test_remote = {
-    animateDelay: 5000,
+    animateDelay: 4500,
+    sendDelay: 500,
     player0: {
         user_id: null,
-        current: 0,
         send_turn: function(index) {
             console.log('player0.send_turn: ' + index);
             remote.settings.user_id = test_remote.player0.user_id;
             remote.settings.player = 0;
             let turn = test_remote.build(test_game.demo0[index]);
-            turn.current = test_remote.player0.current;
             remote.send_turn(turn);
             setTimeout(function() {
-                test_remote.player0.current++;
-            }, 999);
+                game.turn.current++;
+            }, test_remote.sendDelay);
         },
         get_turn: function() {
-            console.log('player0.get_turn: ' + test_remote.player0.current);
+            console.log('player0.get_turn: ' + game.get_current_turn());
             remote.settings.player = 0;
             remote.settings.user_id = test_remote.player0.user_id;
-            game.turn.current = test_remote.player0.current;
             remote.get_turn(remote.settings.timeout_x-1);
         },
     },
     player1: {
         user_id: null,
-        current: 0,
         send_turn: function(index) {
             console.log('player1.send_turn: ' + index);
             remote.settings.user_id = test_remote.player1.user_id;
             remote.settings.player = 1;
             let turn = test_remote.build(test_game.demo1[index]);
-            turn.current = test_remote.player1.current;
             remote.send_turn(turn);
             setTimeout(function() {
-                test_remote.player1.current++;
-            }, 999);
+                game.turn.current++;
+            }, test_remote.sendDelay);
         },
         get_turn: function() {
-            console.log('player1.get_turn: ' + test_remote.player1.current);
+            console.log('player0.get_turn: ' + game.get_current_turn());
             remote.settings.player = 1;
             remote.settings.user_id = test_remote.player1.user_id;
-            game.turn.current = test_remote.player1.current;
             remote.get_turn(remote.settings.timeout_x-1);
         },
     },
@@ -51,31 +46,35 @@ let test_remote = {
             test_remote.player0.send_turn(0);
             setTimeout(function() { // test_remote.player1.send_turn(0);
                 test_remote.player1.get_turn();
-                test_remote.player1.send_turn(0);
-                setTimeout(function() { // test_remote.player0.send_turn(1)
-                    test_remote.player0.get_turn();
-                    test_remote.player0.send_turn(1);
-                    setTimeout(function() { // test_remote.player1.send_turn(1);
-                        test_remote.player1.get_turn();
-                        test_remote.player1.send_turn(1);
-                        setTimeout(function() { // test_remote.player0.send_turn(2)
-                            test_remote.player0.get_turn();
-                            test_remote.player0.send_turn(2);
-                            setTimeout(function() { // test_remote.player1.send_turn(2);
+                setTimeout(function () {
+                    test_remote.player1.send_turn(0);
+                    setTimeout(function() { // test_remote.player0.send_turn(1)
+                        test_remote.player0.get_turn();
+                        setTimeout(function() {
+                            test_remote.player0.send_turn(1);
+                            setTimeout(function() { // test_remote.player1.send_turn(1);
                                 test_remote.player1.get_turn();
-                                test_remote.player1.send_turn(2);
-                                setTimeout(function() { // test_remote.player0.send_turn(3)
+                                test_remote.player1.send_turn(1);
+                                setTimeout(function() { // test_remote.player0.send_turn(2)
                                     test_remote.player0.get_turn();
-                                    test_remote.player0.send_turn(3);
-                                    setTimeout(function() { // test_remote.player1.send_turn(3);
+                                    test_remote.player0.send_turn(2);
+                                    setTimeout(function() { // test_remote.player1.send_turn(2);
                                         test_remote.player1.get_turn();
-                                        test_remote.player1.send_turn(3);
-                                    }, (test_remote.animateDelay)); // test_remote.player1.send_turn(3);
-                                }, test_remote.animateDelay); // test_remote.player0.send_turn(3)
-                            }, (test_remote.animateDelay)); // test_remote.player1.send_turn(2);
-                        }, test_remote.animateDelay); // test_remote.player0.send_turn(2)
-                    }, (test_remote.animateDelay)); // test_remote.player1.send_turn(1);
-                }, test_remote.animateDelay); // test_remote.player0.send_turn(1)
+                                        test_remote.player1.send_turn(2);
+                                        setTimeout(function() { // test_remote.player0.send_turn(3)
+                                            test_remote.player0.get_turn();
+                                            test_remote.player0.send_turn(3);
+                                            setTimeout(function() { // test_remote.player1.send_turn(3);
+                                                test_remote.player1.get_turn();
+                                                test_remote.player1.send_turn(3);
+                                            }, (test_remote.animateDelay)); // test_remote.player1.send_turn(3);
+                                        }, test_remote.animateDelay); // test_remote.player0.send_turn(3)
+                                    }, (test_remote.animateDelay)); // test_remote.player1.send_turn(2);
+                                }, test_remote.animateDelay); // test_remote.player0.send_turn(2)
+                            }, test_remote.sendDelay);
+                        }, (test_remote.animateDelay)); // test_remote.player1.send_turn(1);
+                    }, test_remote.animateDelay); // test_remote.player0.send_turn(1)
+                }, test_remote.sendDelay);
             }, (test_remote.animateDelay)); // test_remote.player1.send_turn(0);
         }, test_remote.animateDelay); // test_remote.player0.send_turn(0)
     },
