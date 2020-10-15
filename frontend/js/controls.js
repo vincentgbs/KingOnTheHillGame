cf0d3b75c384ad489c4c761d37e246604b93510c
vincent <vincent.gbs11@gmail.com>
2020-10-15 08:58:19 -0400

    Adding controls

--- frontend/js/controls.js
+++ frontend/js/controls.js
@@ -1,6 +1,28 @@
 var controls = {
     settings: {
-        //
+        player: -1,
+        start: false,
+    },
+    getCursorPosition: function (ctx, event) {
+        const rect = ctx.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        return {col: Math.floor(x/layout.settings.square_size),
+            row: Math.floor(y/layout.settings.square_size)};
+    },
+    on_click: function(c, e) {
+        let cont = true;
+        if (game.get_current_turn() != controls.settings.player) {
+            console.debug("It's not your turn");
+            cont = false;
+        }
+        if (remote.settings.local ===  true) {
+            cont = true;
+        }
+        if (cont) {
+            let coord = this.getCursorPosition(c, e);
+            console.debug(coord);
+        }
     },
     get_nop: function() {
         game.settings.no_of_players = document.querySelector("#no_of_players").value;
@@ -22,6 +44,10 @@ var controls = {
 
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log('controls.js (2) loaded');
+    const canvas = document.querySelector("#board");
+    canvas.addEventListener('mousedown', function(e) {
+        controls.on_click(canvas, e);
+    });
     if (document.querySelector("#remote_url")) {
         document.querySelector("#remote_url").onkeyup = remote.get_url;
     }
